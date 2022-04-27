1df6960589a66b8e255162f8e6ee1fa8da48f832
Jack Botvinovski <jbotvinovski@gmail.com>
2022-04-27 16:41:37 +0300

    install docz

---
+++ doczrc.js
@@ -0,0 +1,22 @@
+export default {
+  typescript: true,
+  themeConfig: {
+    header: {
+      fixed: true,
+    },
+    logo: {
+      src: {
+        light: '/gravity-logo-dark.svg',
+        dark: '/gravity-logo-light.svg',
+      },
+      width: 200,
+    },
+    menu: {
+      headings: {
+        rightSide: true,
+        scrollspy: true,
+        depth: 3,
+      },
+    },
+  },
+}
